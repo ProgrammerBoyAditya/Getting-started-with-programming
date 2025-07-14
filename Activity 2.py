fd354850294bc8d7f334067203147ffac6566691
ProgrammerBoyAditya <utrejaaditya@gmail.com>
2025-07-14 16:40:07 +0530

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,9 @@
+# printing different variables
+x = 9
+y = "Aditya "
+print(x)
+print(y)
+codingal = "codingal" 
+print (codingal)
+name = input ("Enter your name :")
+print("Hello", name, "\nwelcome to Codingal")
