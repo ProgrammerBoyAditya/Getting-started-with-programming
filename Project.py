a7b5922bbbf888ce22c85f2ac0a195573069ae05
ProgrammerBoyAditya <utrejaaditya@gmail.com>
2025-07-14 17:25:39 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,10 @@
+Dilip = "1st June 2011"
+Kishore = "12th April 2011"
+Khalid = "14th July 2011"
+Fatima ="2nd June 2012"
+Akshay = "13 July 2011" 
+print("Dilip", Dilip)
+print("Kishore", Kishore)
+print("Khalid", Khalid)
+print("Fatima", Fatima)
+print("Akshay", Akshay)
